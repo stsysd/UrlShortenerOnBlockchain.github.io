9877e6997507b687e3e94c30e62993056859487c
stsysd <000stsysd000@gmail.com>
2018-10-04 10:46:19 +0900

    Fix url protocol

--- docs/js/app.js
+++ docs/js/app.js
@@ -150,7 +150,7 @@ let appForm = {
       return this.writable && !this.waiting;
     },
     shortUrl() {
-      return "https://" + location.host + location.pathname + "#" + this.key;
+      return location.protocol + "//" + location.host + location.pathname + "#" + this.key;
     }
   },
 
@@ -211,7 +211,7 @@ let app = new Vue({
       return "http://" + this.text;
     },
     root() {
-      return "https://" + location.host + location.pathname;
+      return location.protocol + "//" + location.host + location.pathname;
     }
   },
 
